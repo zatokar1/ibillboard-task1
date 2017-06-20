e3ff117d27c6a33e2d5282c416cedbc19f9b887a
Ondrej Tokar <maguss.service@gmail.com>
2017-06-20 17:25:07 +0200

    Refactoring after comments from IB

--- tests/main-test.js
+++ tests/main-test.js
@@ -1,15 +1,21 @@
 var chai = require('chai');
 //var expect = chai.expect; // we are using the "expect" style of Chai
 var assert = chai.assert;
-var Main = require('../main');
 
-// Init main module
-var main = new Main();
+var config = require('../config');
+
+// DB
+var DbHandler = require('../dbHandler.js');
+var dbHandler = new DbHandler();
+
+// File
+var FileHandler = require('../fileHandler.js');
+var fileHandler = new FileHandler();
 
 // Truncate log file contents
-describe('main', function() {
+describe('fileHandler', function() {
   it('writeToFile(fileName, value, callback) should write value to log file', function(done) {
-    main.writeToFile(main.logFileName, '', function(err){
+    fileHandler.writeToFile(config.logFileName, '', function(err){
 		assert.ifError(err);
 		done();
 	});
@@ -17,9 +23,9 @@ describe('main', function() {
 });
 
 // Write to DB - Initialize value to 0
-describe('main', function() {
+describe('dbHandler', function() {
   it('writeToRedis(key, value, callback) should write value to DB', function(done) {
-    main.writeToRedis('count', 0, function(err){
+    dbHandler.writeToRedis('count', 0, function(err){
 		assert.ifError(err);
 		done();
 	});
@@ -27,9 +33,9 @@ describe('main', function() {
 });
 
 // Write to log file
-describe('main', function() {
+describe('fileHandler', function() {
   it('appendToFile(fileName, value, callback) should write value to log file', function(done) {
-    main.appendToFile(main.logFileName, 0, function(err){
+    fileHandler.appendToFile(config.logFileName, 0, function(err){
 		assert.ifError(err);
 		done();
 	});
@@ -37,9 +43,9 @@ describe('main', function() {
 });
 
 // Read from DB - expected value 0
-describe('main', function() {
+describe('dbHandler', function() {
   it('readFromRedis(key, callback) should read value from DB', function(done) {
-   main.readFromRedis('count',function(err, value){
+   dbHandler.readFromRedis('count',function(err, value){
 			assert.ifError(err);
 			assert.equal(value, 0);
             done();
@@ -48,9 +54,9 @@ describe('main', function() {
 });
 
 // Incr by 1 in DB
-describe('main', function() {
+describe('dbHandler', function() {
   it('incrByRedis(key, value, callback) should write value to DB', function(done) {
-    main.incrByRedis('count', 1, function(err){
+    dbHandler.incrByRedis('count', 1, function(err){
 		assert.ifError(err);
 		done();
 	});
@@ -58,9 +64,9 @@ describe('main', function() {
 });
 
 // Read from DB - expected value 1
-describe('main', function() {
+describe('dbHandler', function() {
   it('readFromRedis(key, callback) should read value from DB', function(done) {
-   main.readFromRedis('count',function(err, value){
+   dbHandler.readFromRedis('count',function(err, value){
 			assert.ifError(err);
 			assert.equal(value, 1);
             done();
